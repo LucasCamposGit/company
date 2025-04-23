2460189c0a86cc313bdd670e5ce93ca785f02f5a
LucasCamposGit <lucascpn2501@gmail.com>
2025-04-23 12:16:42 -0300

    done

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const funcionariosRoutes = require('./routes/employees');
 const { swaggerUi, specs } = require('./swagger/swagger');
 
 app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/funcionarios', funcionariosRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// Serve os arquivos estáticos do frontend:
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- db.js
+++ db.js
@@ -1,15 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'empresa'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "empresa",
 });
 
-connection.connect(err => {
-  if (err) throw err;
-  console.log('Conectado ao MySQL!');
+connection.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar no banco:", err);
+    return;
+  }
+  console.log("Conectado ao banco de dados MySQL!");
 });
 
 module.exports = connection;
