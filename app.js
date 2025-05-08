9d2294fe6e04ba95aeef111494f999eabdaa9cd9
LucasCamposGit <102190513+LucasCamposGit@users.noreply.github.com>
2025-05-08 09:19:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors')
 const funcionariosRoutes = require('./routes/employees');
 const { swaggerUi, specs } = require('./swagger/swagger');
 
+app.use(cors());
 app.use(express.json());
 app.use('/funcionarios', funcionariosRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
